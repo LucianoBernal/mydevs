84465fed8924947efa3655f4ec1889daab455f4a
LucianoBernal <bernal.luciano@gmail.com>
2014-04-14 22:50:03 -0300

    Segundo commit de Luciano

--- prueba.c
+++ prueba.c
@@ -4,7 +4,7 @@ int main()
 {
 	printf("Hola, soy belen\n");
 	printf("Hola, soy Leandro\n");
-	printf("HOla, soy LUciano\n");
+	printf("Hola, soy Luciano\n");
 	printf("primer commit desde github de Luciano\n");
 			return 0;
 }
