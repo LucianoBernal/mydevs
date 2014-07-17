41ab3ddce412ef461c58ea08f08dfd408a7b7ac6
belentorres <belen.bely@hotmail.com>
2014-07-17 16:19:56 -0300

    cambio nombre syscalls

--- TP_SO/Kernel/syscall.c
+++ TP_SO/Kernel/syscall.c
@@ -7,87 +7,88 @@
 
 #include "Kernel.h"
 #include <string.h>
-/*
- typedef struct {
- int valor;
- t_queue* procesosBloqueados;
- t_sem mutexCola;
- } t_estructuraSemaforo;
+#include <semaphore.h>
 
- t_dictionary* semaforos;
+typedef struct {
+	int valor;
+	t_queue* procesosBloqueados;
+	t_sem mutexCola;
+} t_estructuraSemaforo;
 
- int obtener_valor(char id, int idCpu) {
- sem_wait(mutexVG);
- int a = dictionary_get(variables_globales, &id);
- send(idCpu, a, 4, 0); //TODO
- sem_wait(mutexVG);
- return a;
- }
+t_dictionary* semaforos;
 
- int grabar_valor(char id, int valor) {
- sem_wait(mutexVG);
- dictionary_put(variables_globales, &id, &valor);
- sem_wait(mutexVG);
- return valor;
- }
+int sc_obtener_valor(char id, int idCpu) {
+	sem_wait(mutexVG);
+	int a = dictionary_get(variables_globales, &id);
+	send(idCpu, a, 4, 0); //TODO
+	sem_wait(mutexVG);
+	return a;
+}
 
- void signal(char* idSem) {
- t_estructuraSemaforo* semaforo = dictionary_get(semaforos, idSem);
- semaforo->valor = (semaforo->valor) + 1;
- if (!queue_is_empty(semaforo->procesosBloqueados)) {
- sem_wait(&(semaforo->mutexCola));
- int * programaADesbloquear = queue_pop(semaforo->procesosBloqueados);
- sem_post(&(semaforo->mutexCola));
- int sd = obtener_sd_Programa(*programaADesbloquear);
- send(sd, 's', 1, 0);
- }
- }
+int sc_grabar_valor(char id, int valor) {
+	sem_wait(mutexVG);
+	dictionary_put(variables_globales, &id, &valor);
+	sem_wait(mutexVG);
+	return valor;
+}
 
- void wait(char* idSem, int idCpu) {
- t_estructuraSemaforo* semaforo = dictionary_get(semaforos, idSem);
- int a = semaforo->valor;
- if (a > 0) {
- semaforo->valor = (semaforo->valor) - 1;
- send(idCpu, 's', 1, 0); //TODO
- } else {
- t_estructuraSemaforo* semaforo = dictionary_get(semaforos, idSem);
- int programID = programIdDeCpu(idCpu);
- sem_wait(&(semaforo->mutexCola));
- queue_push(semaforo->procesosBloqueados, &programID);
- sem_post(&(semaforo->mutexCola));
- }
- }
+void sc_signal(char* idSem) {
+	t_estructuraSemaforo* semaforo = dictionary_get(semaforos, idSem);
+	semaforo->valor = (semaforo->valor) + 1;
+	if (!queue_is_empty(semaforo->procesosBloqueados)) {
+		sem_wait(&(semaforo->mutexCola));
+		int * programaADesbloquear = queue_pop(semaforo->procesosBloqueados);
+		sem_post(&(semaforo->mutexCola));
+		int sd = obtener_sd_Programa(*programaADesbloquear);
+		send(sd, 's', 1, 0);
+	}
+}
 
- int imprimir(){
- //??
- }
+void sc_wait(char* idSem, int idCpu) {
+	t_estructuraSemaforo* semaforo = dictionary_get(semaforos, idSem);
+	int a = semaforo->valor;
+	if (a > 0) {
+		semaforo->valor = (semaforo->valor) - 1;
+		send(idCpu, 's', 1, 0); //TODO
+	} else {
+		t_estructuraSemaforo* semaforo = dictionary_get(semaforos, idSem);
+		int programID = programIdDeCpu(idCpu);
+		sem_wait(&(semaforo->mutexCola));
+		queue_push(semaforo->procesosBloqueados, &programID);
+		sem_post(&(semaforo->mutexCola));
+	}
+}
 
- int imprimirTexto(char* texto, int idCpu) {
- int programID = programIdDeCpu(idCpu);
- int sd = obtener_sd_Programa(programID);
- notificar_Programa(sd,texto);
- return strlen(texto);
- }
+int sc_imprimir() {
+	//??
+}
 
- void armarDiccionarioDeSemaforos() {
- int i = 0;
- while (i < cantidadDeSemaforos) {
- t_estructuraSemaforo* semaforo = malloc(sizeof(t_estructuraSemaforo));
- int* valor = list_get(semaforos,i);
- sem_t mutex;
- t_queue* cola = queue_create();
- sem_init(&mutex, 0,1);
- semaforo-> mutex;
- semaforo->procesosBloqueados = cola;
- semaforo->valor = *valor;
- dictionary_put(semaforos, valor, semaforo);
- i++;
- }
- }
+int sc_imprimirTexto(char* texto, int idCpu) {
+	int programID = programIdDeCpu(idCpu);
+	int sd = obtener_sd_Programa(programID);
+	notificar_Programa(sd, texto);
+	return strlen(texto);
+}
 
- int programIdDeCpu(int idCPU) {
- int posicion = posicionEnLaLista(CPUs, idCPU);
- t_estructuraCPU* CPU = list_get(CPUs, posicion);
- return (CPU->idProceso);
+void armarDiccionarioDeSemaforos() {
+	int i = 0;
+	while (i < cantidadDeSemaforos) {
+		t_estructuraSemaforo* semaforo = malloc(sizeof(t_estructuraSemaforo));
+		int* valor = list_get(semaforos, i);
+		sem_t mutex;
+		t_queue* cola = queue_create();
+		sem_init(&mutex, 0, 1);
+		semaforo->mutexCola=mutex;
+		semaforo->procesosBloqueados = cola;
+		semaforo->valor = *valor;
+		dictionary_put(semaforos, valor, semaforo);
+		i++;
+	}
+}
 
- }*/
+int programIdDeCpu(int idCPU) {
+	int posicion = posicionEnLaLista(CPUs, idCPU);
+	t_estructuraCPU* CPU = list_get(CPUs, posicion);
+	return (CPU->idProceso);
+
+}
