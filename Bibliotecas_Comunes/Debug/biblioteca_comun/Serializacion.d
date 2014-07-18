9064f9f9b37cf931a3e871c01854b5f7b02bcaf9
lspir <leandrospir@gmail.com>
2014-07-18 13:55:30 -0300

    Parser nuevo

--- Bibliotecas_Comunes/Debug/biblioteca_comun/Serializacion.d
+++
@@ -1,6 +0,0 @@
-biblioteca_comun/Serializacion.d: ../biblioteca_comun/Serializacion.c \
- ../biblioteca_comun/Serializacion.h ../biblioteca_comun/definiciones.h
-
-../biblioteca_comun/Serializacion.h:
-
-../biblioteca_comun/definiciones.h:
